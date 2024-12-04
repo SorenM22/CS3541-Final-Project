b51d289c3e081fefb8a9bee7c5a6af2f2560c74d
palm0478 <palm0478@d.umn.edu>
2024-12-04 16:16:29 -0600

    created JobPosting data container

---
+++ lib/Presenter/rows.dart
@@ -0,0 +1,18 @@
+class JobPosting {
+  String company;
+  double companyScore;
+  String jobTitle;
+  String location;
+  Duration date;
+  double lowerSalary;
+  double upperSalary;
+
+  JobPosting(
+      this.company,
+      this.companyScore,
+      this.jobTitle,
+      this.location,
+      this.date,
+      this.lowerSalary,
+      this.upperSalary);
+}
